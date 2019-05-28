4b4238b3b4f01e2d9092050f40dec0a49d237718
Wabri <gabriele.puliti@gmail.com>
2019-05-28 15:31:29 +0200

    Update (i3battery): use CommandManager class inside the main script

--- i3battery.py
+++ i3battery.py
@@ -7,6 +7,8 @@ import signal
 import sys
 import time
 
+from helper import Helper
+
 try:
     os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
     from pygame import mixer
@@ -30,60 +32,7 @@ except:
 # ------------ Global variables ---------- #
 ############################################
 
-aspected_arguments = {
-    'pre-config': [
-        '--help'
-    ],
-    'pre-run': [
-        '--test-audio',
-        '--test-notify'
-    ],
-    'config': [
-        '--audio',
-        '--audio-path',
-        '--no-notify',
-        '--wt',
-        '--time',
-        '--power-path',
-        '--bat'
-    ]
-}
-
-############################################
-# ------------ Help printer -------------- #
-############################################
-
-help_info = 'I3battery infos\n\nWeb Repository: https://github.com/Wabri/i3battery\nVersion: 1.0.1\nMantainers: Wabri - Gabriele Puliti'
-
-help_output = {
-    '--help': ['Print all the command and usage', '--help'],
-    '--audio': ['Abilitate audio (default=disabled)', '--audio'],
-    '--audio-path': ['Set the audio path (default=~/.config/i3battery/audio/', '--audio-path=<absolute_path>'],
-    '--no-notify': ['Disabilitate notification (default=abilitated)', '--no-notify'],
-    '--wt': ['Set the warning threshold to different values. You can use a non order list of the value, the scripts order for you. You can set up max 3 values. (default=20,15,5)', '--wt=<value_1>,<value_2>,<value_3>'],
-    '--time': ['Define the time of cycle (default=20)', '--time=<seconds>'],
-    '--power-path': ['Specify the path of the system class power supply (default=/sys/class/power_supply/)', '--power-path=<absolute_path>'],
-    '--bat': ['Specify the battery you want to use (default=BAT0)', '--bat=<battery_name>'],
-    '--test-audio': ['Use it to test the audio notifications', '--test-audio'],
-    '--test-notify': ['Use it to test the notifications functionality', '--test-notify']
-}
-
-help_command_example = 'i3battery --audio --audio-path=/home/wabri/.config/i3battery/audio/ --wt=77,78,76 --time=7 --power-path=/sys/class/power_supply/ --bat=BAT0'
-
-def print_help():
-    print('-'*20)
-    print(help_info)
-    print('-'*20)
-    print()
-    print("Usage: i3battery", end=" ")
-    for key in help_output.keys():
-        print("[{}]".format(help_output[key][1]), end=" ")
-    print("\n\nThese are the common i3battery argument:")
-    for key in help_output.keys():
-        print('   {}   {}'.format(key, help_output[key][0]))
-    print()
-    print("Here it's an example:")
-    print('   {}'.format(help_command_example))
+helper = Helper()
 
 ############################################
 # ------------ Notify manager ------------ #
@@ -127,7 +76,7 @@ signal.signal(signal.SIGINT, signal_handler)
 args = sys.argv[1:]
 
 if aspected_arguments['pre-config'][0] in args:
-    print_help()
+    helper.print_help()
     exit()
 
 print("-"*23)
